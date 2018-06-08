d0b639d7bb8c435727925ac5f4e7ad8a5f424f40
Pratik Mahadev Dimble <31777260+pratikdimble@users.noreply.github.com>
2018-06-08 08:04:47 +0530

    Create EmployeeMasterDAO.java

---
+++ com/erp/dao/EmployeeMasterDAO.java
@@ -0,0 +1,12 @@
+package com.erp.dao;
+
+import java.util.List;
+
+import com.erp.domain.EmployeeMaster;
+
+public interface EmployeeMasterDAO {
+//	public List<EmployeeMaster> loadAllEmployee();
+//	public List<EmployeeMaster> getEmployee(String name);
+	public  int addEmployee(EmployeeMaster emp);
+}
+
